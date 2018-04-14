fb6daec9e465b81236df59829f1afa30068ed469
salazarj <salazarj@seattleu.edu>
2018-04-14 18:34:12 -0400

    removed TODOs

--- js/main.js
+++ js/main.js
@@ -1,40 +1,15 @@
 /* JavaScript for WATS 3020 Sandwich Machine Assignment */
 
-// Step One ///////////////////////////////////////////////////////////
-//
-// Gather data from the user using the prompt() command.
-// Users will fill in each prompt with text.
-// Provide examples where it may help (e.g. show a comma-separated list of
-// toppings to indicate how to specify toppings, etc.)
-//
-// TODO: Prompt the user for what kind of bread they would like.
-// Ideally, that would look something like: "What kind of bread (white, wheat, flat)?"
 let breadOrder = prompt('What kind of bread would you like to order? (White, Wheat, Gluten-Free or flatebread)', 'wheat');
 
-
-// TODO: Prompt the user for what kind of meat(s) they would like.
-// Indicate they should separate multiple items with a comma:
-// "What kind of meat? (Separate meats with a comma if you would like more than one.)"
 let meatOrder = prompt('What kind of meat would you like on your sandwhich? (Separate multiple meats with a comma.)');
 
-
-// TODO: Prompt the user for what kind of toppings they would like.
-// We expect this to be multiple, so ask them to provide you with a
-// comma-separated list using a user friendly prompt.
 let toppingsOrder = prompt('What toppings would you like on your sandwhich? (Separate multiple toppings with a comma.)');
 
-
-// TODO: Prompt the user for what kind of condiments they would like.
-// Again, we should expect a comma-separated list if items here.
 let condimentsOrder = prompt('What condiments would you like on your sandwhich? (Separate multiple condiments with a comma).');
 
 
 // Step Two ////////////////////////////////////////////////////////////
-//
-// Process all the input to calculate values for the user's order. The named
-// values are initialized to `null`. Change that so they equal the proper value.
-//
-// The `prices` object below specifies the prices for each thing.
 
 let prices = {
     sandwich: 5, // Base price for a sandwich is $5, includes bread
@@ -43,38 +18,24 @@ let prices = {
     condiment: 0.25 // Each condiment costs $0.25
 };
 
-// TODO: Convert order information from Strings to Arrays.
 
 let meatArray = meatOrder.split(',');
 let toppingArray = toppingsOrder.split(',');
 let condimentArray = condimentsOrder.split(',');
 
-// TODO: Calculate cost for meat, toppings, and condiments.
-// This requires you to determine the length of each Array you just made
-// and multiply out the costs. You will need to refer to the attributes of the
-// `prices` object in order to calculate these costs.
-
 let meatCost = meatArray.length * prices.meat;
 let toppingCost = toppingArray.length * prices.topping;
 let condimentCost = condimentArray.length * prices.condiment;
 
-// TODO: Combine the costs of each part of the sandwich to get the subtotal.
 let subtotal = prices.sandwich + meatCost + toppingCost + condimentCost;
 
-// TODO: Calculate the tax owed using the waStateTaxRate.
 let waStateTaxRate = 0.065;
 let orderTax = subtotal * waStateTaxRate;
 
-// TODO: Calculate `totalPrice` by adding `subtotal` and `orderTax`.
 let totalPrice = subtotal + orderTax;
 
 
 // Step Three //////////////////////////////////////////////////////////
-//
-// TODO: Now that we've calculated all the values, insert them into this
-// template literal using proper expression tags. Note that we must provide
-// all of the info the user gave us to confirm the order, and then we must also
-// provide the cost information so the user understands their bill.
 
 let receiptTemplate = `
     <p>SANDWICH ORDER</p>
@@ -95,7 +56,7 @@ let receiptTemplate = `
 `
 
 ///////////////////////////////////////////////////////////////////////
-// Do not edit below this line unless you are doing something fancy!
+// Fancy Stuf!!
 //////////////////////////////////////////////////////////////////////
 let receiptText = document.querySelector("#receipt-text");
 receiptText.innerHTML = receiptTemplate;
